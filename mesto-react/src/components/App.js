606c0f409da8ab55ae2a4b267bedab1d5b6c4c8d
Григорьева Марина <mgrigoreva@phoenixit.ru>
2022-06-14 10:41:24 +0300

    fix popup

--- mesto-react/src/components/App.js
+++ mesto-react/src/components/App.js
@@ -49,7 +49,7 @@ function App() {
       <Main onAddPlace={handleAddPlaceClick} onEditProfile={handleEditProfileClick} 
         onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick}/>
       <Footer />
-      {isEditProfilePopupOpen && <PopupWithForm popupHeader="Редактировать профиль" popupName="edit-pop-up"
+      <PopupWithForm popupHeader="Редактировать профиль" popupName="edit-pop-up"
         formName="profile-edit-form" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
         <label className="form__field">
           <input type="text" id="name" name="name" placeholder="Имя" className="form__input" required minLength="2" maxLength="40" />
@@ -59,16 +59,16 @@ function App() {
           <input type="text" id="activity" name="activity" placeholder="О себе" className="form__input" required minLength="2" maxLength="200" />
           <span className="activity-input-error form__input-error"></span>
         </label>
-      </PopupWithForm>}
-      {isEditAvatarPopupOpen && <PopupWithForm popupHeader="Обновить аватар" popupName="edit-avatar-pop-up" 
+      </PopupWithForm>
+      <PopupWithForm popupHeader="Обновить аватар" popupName="edit-avatar-pop-up" 
         formName="avatar-edit-form" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
           <label className="form__field">
             <input type="url" id="ava-link" name="link" placeholder="Ссылка на аву" className="form__input" required />
             <span className="link-input-error form__input-error"></span>
           </label>
-        </PopupWithForm>}
+        </PopupWithForm>
 
-      {isAddPlacePopupOpen && <PopupWithForm popupHeader="Новое место" popupName="add-card-pop-up"
+      <PopupWithForm popupHeader="Новое место" popupName="add-card-pop-up"
         formName="add-card-form" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
         <label className="form__field">
           <input type="text" id="place" name="name" placeholder="Название" className="form__input" required minLength="2" maxLength="30" />
@@ -78,12 +78,9 @@ function App() {
           <input type="url" id="link" name="link" placeholder="Ссылка на картинку" className="form__input" required />
           <span className="link-input-error form__input-error"></span>
         </label>
-      </PopupWithForm>}
+      </PopupWithForm>
       <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
       <ConfirmPopup title="Вы уверены?" btnText="Да" />
-
-
-
     </div>
   );
 }
