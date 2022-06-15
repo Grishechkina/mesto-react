1ed18ea55b8e22002754289ff4767e4b77262b50
Григорьева Марина <mgrigoreva@phoenixit.ru>
2022-06-15 12:52:19 +0300

    fix 1st review

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import ImagePopup from './ImagePopup'
 import ConfirmPopup from './ConfirmPopup'
 
 function App() {
+
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
@@ -26,60 +27,65 @@ function App() {
   function handleCardClick(card) {
     setSelectedCard(card)
   }
-  function closeAllPopups(event) {
-    if(event.target.classList.contains('pop-up_opened') || event.target.classList.contains('pop-up__close-btn')) {
-      if(isEditProfilePopupOpen) {
-      return setIsEditProfilePopupOpen(false)
-    }
-    if(isEditAvatarPopupOpen) {
-      return setIsEditAvatarPopupOpen(false)
-    }
-    if(isAddPlacePopupOpen) {
-      return setIsAddPlacePopupOpen(false)
-    }
-    if(Object.keys(selectedCard).length > 0) {
-      return setSelectedCard({})
-    }
-    }
-    
+  function closeAllPopups() {
+    setIsEditProfilePopupOpen(false)
+    setIsEditAvatarPopupOpen(false)
+    setIsAddPlacePopupOpen(false)
+    setSelectedCard({})
   }
+
   return (
     <div className="page">
       <Header />
-      <Main onAddPlace={handleAddPlaceClick} onEditProfile={handleEditProfileClick} 
-        onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick}/>
+      <Main 
+        onAddPlace={handleAddPlaceClick} 
+        onEditProfile={handleEditProfileClick}
+        onEditAvatar={handleEditAvatarClick} 
+        onCardClick={handleCardClick} />
       <Footer />
-      <PopupWithForm popupHeader="Редактировать профиль" popupName="edit-pop-up"
-        formName="profile-edit-form" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+      <PopupWithForm 
+        popupHeader="Редактировать профиль" 
+        popupName="edit-pop-up"
+        formName="profile-edit-form" 
+        isOpen={isEditProfilePopupOpen} 
+        onClose={closeAllPopups}>
         <label className="form__field">
           <input type="text" id="name" name="name" placeholder="Имя" className="form__input" required minLength="2" maxLength="40" />
-          <span className="name-input-error form__input-error"></span>
+          <span className="name-input-error form__input-error"/>
         </label>
         <label className="form__field">
           <input type="text" id="activity" name="activity" placeholder="О себе" className="form__input" required minLength="2" maxLength="200" />
-          <span className="activity-input-error form__input-error"></span>
+          <span className="activity-input-error form__input-error"/>
         </label>
       </PopupWithForm>
-      <PopupWithForm popupHeader="Обновить аватар" popupName="edit-avatar-pop-up" 
-        formName="avatar-edit-form" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
-          <label className="form__field">
-            <input type="url" id="ava-link" name="link" placeholder="Ссылка на аву" className="form__input" required />
-            <span className="link-input-error form__input-error"></span>
-          </label>
-        </PopupWithForm>
+      <PopupWithForm 
+        popupHeader="Обновить аватар" 
+        popupName="edit-avatar-pop-up"
+        formName="avatar-edit-form"
+        isOpen={isEditAvatarPopupOpen} 
+        onClose={closeAllPopups}>
+        <label className="form__field">
+          <input type="url" id="ava-link" name="link" placeholder="Ссылка на аву" className="form__input" required />
+          <span className="link-input-error form__input-error"/>
+        </label>
+      </PopupWithForm>
 
-      <PopupWithForm popupHeader="Новое место" popupName="add-card-pop-up"
-        formName="add-card-form" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+      <PopupWithForm 
+        popupHeader="Новое место" 
+        popupName="add-card-pop-up"
+        formName="add-card-form" 
+        isOpen={isAddPlacePopupOpen} 
+        onClose={closeAllPopups}>
         <label className="form__field">
           <input type="text" id="place" name="name" placeholder="Название" className="form__input" required minLength="2" maxLength="30" />
-          <span className="name-input-error form__input-error"></span>
+          <span className="name-input-error form__input-error"/>
         </label>
         <label className="form__field">
           <input type="url" id="link" name="link" placeholder="Ссылка на картинку" className="form__input" required />
-          <span className="link-input-error form__input-error"></span>
+          <span className="link-input-error form__input-error"/>
         </label>
       </PopupWithForm>
-      <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
+      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
       <ConfirmPopup title="Вы уверены?" btnText="Да" />
     </div>
   );
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 function PopupWithForm(props) {
+
+  function closeByOverlay(event) {
+    if (event.target.classList.contains('pop-up_opened')) {
+      props.onClose()
+    }
+  }
+
   return (
-    <div className={`pop-up ${props.popupName}${props.isOpen ? ' pop-up_opened' : ''}`} onClick={props.onClose}>
+    <div className={`pop-up ${props.popupName}${props.isOpen ? ' pop-up_opened' : ''}`} onClick={closeByOverlay}>
       <div className="pop-up__container" >
-        <button className="pop-up__close-btn" onClick={props.onClose}></button>
+        <button className="pop-up__close-btn" onClick={props.onClose}/>
         <h2 className="pop-up__title">{props.popupHeader}</h2>
         <form className={`pop-up__${props.formName} form`} name={props.formName} noValidate>
           {props.children}
